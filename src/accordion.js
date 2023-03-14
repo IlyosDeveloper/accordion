6ccb2dc51da834cdf6c037c6eb3c3485228b7ff2
IlyosAxmadAliy <ilyosaxmadaliy@gmail.com>
2023-03-14 16:19:33 +0500

    accordion

---
+++ src/accordion.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+function Accordion({ title, text }) {
+  const [show, setShow] = useState(false);
+
+  return (
+    <div className='accordion'>
+      <div className='header'>
+        <h2 className='title'>{title}</h2>
+        <button onClick={() => setShow(!show)}>{show ? "-" : "+"}</button>
+      </div>
+      {show && <p className="text">{text}</p>}
+    </div>
+  );
+}
+
+export default Accordion;
